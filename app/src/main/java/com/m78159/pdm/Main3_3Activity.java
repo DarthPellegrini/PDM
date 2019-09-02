9150251186d212d9a08d9d72dbe3fdfbdfbd8653
DarthPellegrini <pellegrinidev@outlook.com>
2019-09-02 00:37:36 -0300

    Aula 3 pratically done

--- app/src/main/java/com/m78159/pdm/Main3_3Activity.java
+++ app/src/main/java/com/m78159/pdm/Main3_3Activity.java
@@ -1,5 +1,7 @@
 package com.m78159.pdm;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,13 @@ public class Main3_3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3_3);
+
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.quiz_transito_icon_shrank);
+        getSupportActionBar().setTitle(R.string.app_3_3_title);
+        getSupportActionBar().setBackgroundDrawable(
+                new ColorDrawable(Color.parseColor("#1e1e1e")));
 
     }
 
--- app/src/main/java/com/m78159/pdm/Main3_4Activity.java
+++ app/src/main/java/com/m78159/pdm/Main3_4Activity.java
@@ -1,5 +1,7 @@
 package com.m78159.pdm;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,9 @@ public class Main3_4Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3_4);
 
+        getSupportActionBar().setTitle(R.string.app_3_4_title);
+        getSupportActionBar().setBackgroundDrawable(
+                new ColorDrawable(Color.parseColor("#999999")));
     }
 
 }
