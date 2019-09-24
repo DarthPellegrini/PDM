b8fdd20712854bdda896eb2da93ce8980c387458
DarthPellegrini <pellegrinidev@outlook.com>
2019-09-23 21:14:47 -0300

    Aula 6 done - hotfix

--- app/src/main/java/com/m78159/pdm/Main6_2Activity.java
+++ app/src/main/java/com/m78159/pdm/Main6_2Activity.java
@@ -2,8 +2,6 @@ package com.m78159.pdm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -12,19 +10,18 @@ import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import java.util.Arrays;
 
 public class Main6_2Activity extends AppCompatActivity {
 
     Spinner spinner;
     ListView listView;
-    String estados[] = {"Rio Grande do Sul", "Santa Catarina", "Paraná", "São Paulo", "Rio de Janeiro"};
-    String cidades[][] = {
-            {"Porto Alegre","Rio Pardo","Santa Cruz do Sul","Pantano Grande","Dom Feliciano"},
-            {"Florianópolis","Passo de Torres","Vacaria","Tubarão","Criciúma"},
-            {"Curitiba","Ponta Grossa","Cascavél","Joinville","Paranaguá"},
-            {"São Paulo","Guarujá","Campinas","Bauru","Araraquara"},
-            {"Rio de Janeiro","Teresópolis","Saquarema","Cabo Frio","Cordeiro"}};
+    final String[] estates = {"Rio Grande do Sul", "Santa Catarina", "Paraná", "São Paulo", "Rio de Janeiro"};
+    final String[][] cities = {
+            {"Porto Alegre", "Rio Pardo", "Santa Cruz do Sul", "Pantano Grande", "Dom Feliciano"},
+            {"Florianópolis", "Passo de Torres", "Vacaria", "Tubarão", "Criciúma"},
+            {"Curitiba", "Ponta Grossa", "Cascavél", "Joinville", "Paranaguá"},
+            {"São Paulo", "Guarujá", "Campinas", "Bauru", "Araraquara"},
+            {"Rio de Janeiro", "Teresópolis", "Saquarema", "Cabo Frio", "Cordeiro"}};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,16 +32,16 @@ public class Main6_2Activity extends AppCompatActivity {
         final TextView t = findViewById(R.id.nome);
         t.setText(nome);
 
-        spinner = (Spinner) findViewById(R.id.spinnerEstado);
-        listView =  (ListView) findViewById(R.id.listviewCidade);
+        spinner = findViewById(R.id.spinnerEstado);
+        listView = findViewById(R.id.listviewCidade);
 
-        ArrayAdapter<String> adapter_sp = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, estados);
+        ArrayAdapter<String> adapter_sp = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, estates);
         spinner.setAdapter(adapter_sp);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
                 ArrayAdapter<String> adapter_lst =
-                        new ArrayAdapter<>(getApplicationContext(), R.layout.list_layout, R.id.textViewList, cidades[position]);
+                        new ArrayAdapter<>(getApplicationContext(), R.layout.list_layout, R.id.textViewList, cities[position]);
                 listView.setAdapter(adapter_lst);
             }
 
